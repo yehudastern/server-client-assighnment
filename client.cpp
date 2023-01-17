db5f8273a22d05c35c1f2fbadd0ba28e6a6d1ce5
yehudastern <yehudastern@gamil.com>
2023-01-18 00:30:16 +0200

    change client

--- client.cpp
+++ client.cpp
@@ -7,13 +7,45 @@ int main(int argc, char** argv) {
         cout << "please run again with argument" << endl;
         exit(1);
     }
+    int num;
+    char s[4096];
     ClientClass client(argv[1], argv[2]); // starts the client
     client.connect(); // connects to the server
     while(true) {
-        char data_addr[4090];
-        // takes a message from the user
-        scanf(" %[^\n]", data_addr);
-        client.sendMessage(data_addr); // sends a message
-        cout << client.receiveMessage() << endl; // prints the message from the server
+        // prints the message from the server
+        for (size_t i = 0; i < 5; i++) {
+            cout << client.receiveMessage() << endl;
+        }
+        cin >> num;
+        switch (num){
+        case 1:
+            client.sendMessage("1");
+            cout << client.receiveMessage() << endl;
+            uploadCls();
+            cout << client.receiveMessage() << endl;
+            uploadUnCls();
+            break;
+        case 2:
+            client.sendMessage("2");
+            cin.getline(s, 4096);
+            client.sendMessage(s);
+            break;
+        case 3:
+            client.sendMessage("3");
+            cin.getline(s, 4096);
+            client.sendMessage(s); 
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 8:
+            client.sendMessage("-1");
+            break;
+        default:
+            cout << "invaild input!" <<endl;
+            break;
+        }
     }
 }
