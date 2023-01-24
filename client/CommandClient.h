ac3cbbbe6ac5e86c6f9f19962d0d6b1df5fce81d
yehudastern <109284899+yehudastern@users.noreply.github.com>
2023-01-24 22:37:29 +0200

    Update CommandClient.h

--- client/CommandClient.h
+++ client/CommandClient.h
@@ -9,7 +9,14 @@ protected:
 public:
     virtual void execute() = 0;
 };
+/*
+    file for all the command, using command pattern to execute the knn algorithm
+*/
 
+/*
+    The "upload data" command is used to upload a dataset to the K-nearest neighbors (KNN) algorithm.
+    performance check to the path.
+*/  
 class UploadCommandClient : public CommandClient {
     public:
     UploadCommandClient(DefaultIOCli*);
@@ -17,24 +24,37 @@ class UploadCommandClient : public CommandClient {
     void execute();
 };
 
+/*
+    set k for K-nearest neighbors (KNN) algorithm, and distance for calculate the nearest vectors,
+    performance some check input to vaildete the k and distance is vaild
+*/
 class AlgorithmSettingCommandClient : public CommandClient {
     public:
     AlgorithmSettingCommandClient(DefaultIOCli*);
     void execute();
 };
 
+/*
+    classify our unclassified data by our classified data using out seting and knn class
+*/
 class ClassifyCommandClient : public CommandClient {
     public:
     ClassifyCommandClient(DefaultIOCli*);
     void execute();
 };
 
+/*
+    get all the tagged that classified from server and print them to the screen.
+*/
 class DisplayCommandClient : public CommandClient {
     public:
     DisplayCommandClient(DefaultIOCli*);
     void execute();
 };
 
+/*
+    get all the tagged that classified from server and write them to the file.
+*/
 class DownloadCommandClient : public CommandClient {
     public:
     DownloadCommandClient(DefaultIOCli*);
