65247181d6df5f3d2cdad1d3ce74abffc0dbf1e5
yehudastern <109284899+yehudastern@users.noreply.github.com>
2023-01-24 22:33:54 +0200

    Update client.cpp

--- client/client.cpp
+++ client/client.cpp
@@ -6,18 +6,22 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+    // check that we get a vaild number of argument
     if (argc != 3) {
         cout << "please run again with argument" << endl;
         exit(1);
     }
     int num;
-    // starts the client
+    // create a client class
     ClientClass* client = new ClientClass(argv[1], argv[2]);
     // connects to the server
     client->connect(); 
     SocketIOCli dio(client);
+    // creat the manage of the client
     CliClient cliClient(&dio);
+    // beggin the flow of the program
     cliClient.start();
+    // close the connection with the server
     client->closeConnection();
     delete client;
 }
