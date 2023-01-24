6078273040dc3b9e5e824aff6b26411ca84ee59e
yehudastern <109284899+yehudastern@users.noreply.github.com>
2023-01-24 22:40:27 +0200

    Update SocketIOCli.h

--- client/SocketIOCli.h
+++ client/SocketIOCli.h
@@ -12,16 +12,21 @@
 #include <vector>
 #include "ClientClass.h"
 
+/*
+    socketIO He is responsible for communicating with the server,
+    has a communication field and another field for saving the information we receive from the server.
+*/
 class SocketIOCli: public DefaultIOCli{
 private:
     ClientClass* m_server;
     vector<string> m_v;
 public:
     SocketIOCli(ClientClass*);
+    // get data from the user
     virtual std::string read();
+    // send data to the user
     virtual void write(std::string);
     void close();
 };
-
 
 #endif
